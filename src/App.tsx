85299352b065746c78a69bcf5d78ed15fd40d8bc
pinllipho0624-creator <pinllipho0624@gmail.com>
2026-05-29 12:53:51 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './Home.tsx';
+import Home from './Home';
 import About from './About';
 import Contact from './Contact';
 import Privacy from './Privacy';
